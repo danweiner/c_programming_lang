74d2b7242dd396b82808096c777c262313a5ef15
Dan Weiner <daweiner16@gmail.com>
2018-11-14 11:24:12 -0500

    added folder for tutorial

---
+++ tutorial/hello/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 				/* Include info about the standard library */
+
+/* Prints 'hello, world' */
+
+main()							/* Define a function called main that receives no argument values*/
+{								/* Statements of main are enclosed in braces */
+
+	printf("hello, world\n");	/* main calls library function printf to print this sequence of chars*/
+
+}								/* \n represents the newline character */
+
+
+
+
+
+
