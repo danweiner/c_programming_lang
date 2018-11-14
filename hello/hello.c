022c3de498f8faf436e120002504e456948b366d
Dan Weiner <daweiner16@gmail.com>
2018-11-14 11:22:24 -0500

    add hello_printf

--- hello/hello.c
+++ hello/hello.c
@@ -4,5 +4,13 @@
 
 main()							/* Define a function called main that receives no argument values*/
 {								/* Statements of main are enclosed in braces */
+
 	printf("hello, world\n");	/* main calls library function printf to print this sequence of chars*/
-}								/* \n represents the newline character */ 
+
+}								/* \n represents the newline character */
+
+
+
+
+
+
---
+++ hello/hello_printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Prints hello, world on one line.  Shows that printf does not automatically 
+	provide newline character */
+
+main()
+{
+
+	printf("hello, ");
+	printf("world");
+	printf("\n");
+
+}
