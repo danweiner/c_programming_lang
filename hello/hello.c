8e7e2a38036b371dd28379434a134f783016353b
Dan Weiner <daweiner16@gmail.com>
2018-11-14 11:14:20 -0500

    add comments to hello

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> 				/* Include info about the standard library */
 
-main()
-{
-	printf("hello, world\n");
-}
+/* Prints 'hello, world' */
+
+main()							/* Define a function called main that receives no argument values*/
+{								/* Statements of main are enclosed in braces */
+	printf("hello, world\n");	/* main calls library function printf to print this sequence of chars*/
+}								/* \n represents the newline character */ 
